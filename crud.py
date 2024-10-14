449e8ea3258ebfc82b073277cde67b5385aaf249
InduManimaran <indu3102@gmail.com>
2024-10-14 16:52:33 +0530

    create and select user

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@
 from sqlmodel import Field, Session, SQLModel, create_engine,select
 
 class User(SQLModel,table = True):
-    id: int 
+    id: int | None = Field(default=None, primary_key=True)
     username:str
     fullname:str
     email:str 
--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/{name}")
 async def read_user(name: str):
     return {"Hello": name}
+
+
+@app.post("/users/")
+def create_user(user: User):
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+
+
+@app.get("/users/")
+def read_users():
+    with Session(engine) as session:
+        users = session.exec(select(User)).all()
+        return users
